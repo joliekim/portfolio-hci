cf74b6fe573d4823c7a48f4d2701126c63c14a51
jolie <jolieheejikim@gmail.com>
2025-04-23 13:49:33 +0900

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://joliekim.github.io/',
+  base: '/',
   server: {
     port: 3001
   }
